bb9c2e63c24cba2a007bed22a529bda24be44fc4
Roman Zavelev <rzav@mail.ru>
2022-04-29 17:27:56 +0300

    Prepare to HW0

--- src/main/java/ru/javawebinar/topjava/Main.java
+++ src/main/java/ru/javawebinar/topjava/Main.java
@@ -4,7 +4,7 @@ package ru.javawebinar.topjava;
  * @see <a href="http://topjava.herokuapp.com">Demo application</a>
  * @see <a href="https://github.com/JavaOPs/topjava">Initial project</a>
  */
-public class Main {
+public class  Main {
     public static void main(String[] args) {
         System.out.format("Hello TopJava Enterprise!");
     }
